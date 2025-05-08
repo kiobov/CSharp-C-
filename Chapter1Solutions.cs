d1415898d464f987c18dec667902cbe387f76b13
kiobov <nyakio19roseary@gmail.com>
2025-05-08 13:12:48 +0300

    Went through documentation and solved solutions

---
+++ Chapter1Solutions.cs
@@ -0,0 +1,67 @@
+﻿/*//1. Modify the sample program to print a different greeting, for example  "Good Day!".
+
+using System;
+
+class Greeting
+{
+    static void Main()
+    {
+        string name = "Jane";
+        Console.WriteLine("Hello {0}. Thank you for visiting. Have a good Day.", name);
+    }
+}*/
+
+/*/ 2. Write a console application that prints your first and last name on the console.
+class Fullname
+{
+    static void Main()
+    {
+        Console.Write("First name: ");
+        string firstName = Console.ReadLine();
+        Console.Write("Second name: ");
+        string secondName = Console.ReadLine();
+
+        Console.WriteLine("My name is {0} {1} nice to meet you.", firstName, secondName);
+    }
+}*/
+
+/*3. /Write a program that prints the following numbers on the console 1, 101, 1001, each on a new line.
+
+class numbersinNewLine
+{
+    static void Main()
+    {
+        int num1 = 1;
+        int num2 = 101;
+        int num3 = 1001;
+
+        Console.WriteLine(num1);
+        Console.WriteLine(num2);
+        Console.WriteLine(num3);
+    }
+    
+
+}*/
+
+/*4. / Write a program that prints on the console the current date and time.
+Console.WriteLine(DateTime.Now);*/
+
+/*5. / Write a program that prints the square root of 12345. 
+
+
+class SquareRoot
+{
+    static void Main()
+    {
+        double number1 = 1;
+        double number2 = 2;
+        double number3 = 3;
+        double number4 = 4;
+        double number5 = 5;
+        Console.WriteLine(Math.Round(Math.Sqrt(number1), 2));
+        Console.WriteLine(Math.Round(Math.Sqrt(number2), 2));
+        Console.WriteLine(Math.Round(Math.Sqrt(number3), 2));
+        Console.WriteLine(Math.Round(Math.Sqrt(number4), 2));
+        Console.WriteLine(Math.Round(Math.Sqrt(number5), 2));
+    }
+}*/
